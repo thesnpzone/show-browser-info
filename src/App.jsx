0268137833c16ffba402c3b6def049ee1154473d
snpzone <65709045+snpzone@users.noreply.github.com>
2023-04-25 20:23:38 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
 
-      <h1 style={{textAlign:"center"}}>{browserName} Browser</h1>
+      <h1 style={{textAlign:"center"}}>Your Useing <u>{browserName}</u> Browser</h1>
 
     </>
   )
